6d4cd933d354a6aac47af6a97fdbfdd18a6df117
emmastyler <emmastyler1998@gmail.com>
2021-11-16 22:04:08 +0100

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+   
+  })
+  .then(() => console.log("DB Connection Successfull"))
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.use(express.json());
+
+app.use("/server/auth", authRoute);
+app.use("/server/users", userRoute);
+app.use("/server/movies", movieRoute);
+app.use("/server/lists", listRoute);
+
+
+
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
