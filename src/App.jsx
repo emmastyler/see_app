20c270f7cb1d6090cd4fbe8bdc8747a3ee994b55
emmastyler <emmastyler1998@gmail.com>
2021-11-17 23:40:22 +0100

    update client ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
-import "./app.scss"
+import "./app.scss";
 import Home from "./pages/home/Home";
 import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
 import Login from "./pages/login/Login";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  
+} from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./authContext/AuthContext";
 
 const App = () => {
-  return <Register/>;
+  const { user } = useContext(AuthContext);
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {user ? <Home /> : <Register />}
+        </Route>
+        <Route path="/register">
+          {!user ? <Register /> : <Home />}
+        </Route>
+        <Route path="/login">{!user ? <Login /> : <Home />}</Route>
+        {user && (
+          <>
+            <Route path="/movies">
+              <Home type="movie" />
+            </Route>
+            <Route path="/series">
+              <Home type="series" />
+            </Route>
+            <Route path="/watch">
+              <Watch />
+            </Route>
+          </>
+        )}
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
